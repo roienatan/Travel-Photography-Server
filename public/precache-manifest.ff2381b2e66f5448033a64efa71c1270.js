89292518f8f562a227d77a9cac288f5ec9bf1dc1
Roie Natan <roie.natan@gmail.com>
2021-02-19 19:44:30 +0200

    first init for deployement

---
+++ public/precache-manifest.ff2381b2e66f5448033a64efa71c1270.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e821d6dfdd0a825667ee2c14d14939d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a95bfbf3d5f479979810",
+    "url": "/static/css/main.92ff52b5.chunk.css"
+  },
+  {
+    "revision": "8ba97ea86f1fb8c6e08b",
+    "url": "/static/js/2.2c831f9c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2c831f9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a95bfbf3d5f479979810",
+    "url": "/static/js/main.a718e106.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "62617b94c915c3cbf3b9abbcf5c515a0",
+    "url": "/static/media/BluuNext-Bold.62617b94.otf"
+  },
+  {
+    "revision": "f0db446d529c29600575714ebef1d14d",
+    "url": "/static/media/loading.f0db446d.svg"
+  },
+  {
+    "revision": "dcfa36474f2101b46594f0350f6db143",
+    "url": "/static/media/roie-natan.dcfa3647.png"
+  }
+]);
