9d82a62c504576ccb18f81b6424a944fe949f38c
Roie Natan <roie.natan@gmail.com>
2021-02-19 20:23:12 +0200

    revert

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var imagesRouter = require('./routes/images');
 var app = express();
 
 // view engine setup
-app.use(express.static(path.join(__dirname, '/public')));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 const allowCrossDomain = function (req, res, next) {
@@ -27,9 +27,7 @@ app.use(express.json());
 app.use(allowCrossDomain);
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/public/index.html'));
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/authenticate', loginRouter);
