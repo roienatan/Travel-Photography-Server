336535c60d5b93f728be9fa328c07c86aa524c45
Roie Natan <roie.natan@gmail.com>
2021-02-20 14:37:26 +0200

    .

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(allowCrossDomain);
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public/index.html')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/authenticate', loginRouter);
