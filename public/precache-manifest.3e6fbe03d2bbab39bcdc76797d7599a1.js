31efc15061add3a161109fc73f2bf5e916da6704
Roie Natan <roie.natan@gmail.com>
2021-02-20 14:41:06 +0200

    updated

---
+++ public/precache-manifest.3e6fbe03d2bbab39bcdc76797d7599a1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cf9fb1d7897577f3e7ef223a70651e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04111f6d5e802aa71377",
+    "url": "/static/css/main.c89fff8e.chunk.css"
+  },
+  {
+    "revision": "dae520a324f9fd4ad586",
+    "url": "/static/js/2.bb724364.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bb724364.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04111f6d5e802aa71377",
+    "url": "/static/js/main.37d1f1d9.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "62617b94c915c3cbf3b9abbcf5c515a0",
+    "url": "/static/media/BluuNext-Bold.62617b94.otf"
+  },
+  {
+    "revision": "f0db446d529c29600575714ebef1d14d",
+    "url": "/static/media/loading.f0db446d.svg"
+  },
+  {
+    "revision": "dcfa36474f2101b46594f0350f6db143",
+    "url": "/static/media/roie-natan.dcfa3647.png"
+  }
+]);
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.798e750942e05eb844a354a49609b5a4.js"
+  "/precache-manifest.3e6fbe03d2bbab39bcdc76797d7599a1.js"
 );
 
 self.addEventListener('message', (event) => {
